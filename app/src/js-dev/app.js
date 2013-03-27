cd4551deff24e0aae4010a241693e13657aef211
Thomas Degry <degry.thomas@gmail.com>
2013-03-27 19:00:59 +0100

    Loading screen af en logo anim

--- app/src/js-dev/app.js
+++ app/src/js-dev/app.js
@@ -52,12 +52,30 @@
         setTimeout(function() {
             $("#overlay").fadeOut(400);
             $(".sidebar").css('opacity', '0');
+            $(".boat-container").css({'opacity': '0', 'top': '50px'});
+            $(".logo-front").css('opacity', '0');
+
+            
+
             $("#app-wrapper").css({'opacity': '0'}).animate({
                 'opacity' : '1',
             }, 1500, function() {
                 $(".sidebar").css({'opacity': '1'});
                 $('body').css('background-image', 'none');
+                
             });
+
+            $(".logo-front").animate({
+                'opacity' : '1'
+            }, 1500, function() {
+                $(".boat-container").animate({
+                    'opacity': '1',
+                    'top': '43px'
+                });
+            });
+
+            
+
         }, 0);
     });
 
