398b3fa64fa13fc36f20207a55d55392027f3c1d
Tatiana Van Campenhout <tatiana.van.campenhout@gmail.com>
2013-03-29 14:32:20 +0100

    css fixes, checkboxes, select, pull

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -746,8 +746,9 @@ var RolstendeMap = (function () {
 var Settings =(function () {
 
     var Settings = function () {
-        this.api = 'http://192.168.2.9/Devine/_MAMP_JAAR2/_SEM2/MAIV/rolstende/api/'
+        //this.api = 'http://192.168.2.9/Devine/_MAMP_JAAR2/_SEM2/MAIV/rolstende/api/'
         //this.api = 'http://192.168.2.8/maiv_oostende/api/';
+        this.api = 'http://192.168.2.4/rolstende/api/';
     };
 
     return Settings;
