7b99b4d0132af986ac2ce5c68a49590e05506505
Pieter Beulque <pieterbeulque@gmail.com>
2013-03-29 19:40:08 +0100

    Desktop fixes

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -836,8 +836,8 @@ var Settings =(function () {
 
     var Settings = function () {
 
-        this.api = 'http://192.168.1.113/Devine/_MAMP_JAAR2/_SEM2/MAIV/rolstende/api/'
-        //this.api = 'http://192.168.2.8/maiv_oostende/api/';
+        // this.api = 'http://192.168.1.113/Devine/_MAMP_JAAR2/_SEM2/MAIV/rolstende/api/'
+        this.api = 'http://192.168.2.8/maiv_oostende/api/';
         //this.api = 'http://192.168.2.4/rolstende/api/';
     };
 
