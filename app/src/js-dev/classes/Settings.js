daaf04cafe0a37afe09d248d690dda8d56f3b4b7
Pieter Beulque <pieterbeulque@gmail.com>
2013-03-29 11:32:59 +0100

    Sprite

--- app/src/js-dev/classes/Settings.js
+++ app/src/js-dev/classes/Settings.js
@@ -1,12 +1,8 @@
 var Settings =(function () {
 
     var Settings = function () {
-<<<<<<< HEAD
-        this.api = 'http://192.168.2.9/Devine/_MAMP_JAAR2/_SEM2/MAIV/rolstende/api/'
-=======
         // this.api = 'http://192.168.2.9/Devine/_MAMP_JAAR2/_SEM2/MAIV/rolstende/api/'
-        this.api = 'http://192.168.2.4/rolstende/api/';
->>>>>>> 2e00fb2454a31736c7e2d29315714919686d4fb5
+        this.api = 'http://192.168.2.8/maiv_oostende/api/';
     };
 
     return Settings;
